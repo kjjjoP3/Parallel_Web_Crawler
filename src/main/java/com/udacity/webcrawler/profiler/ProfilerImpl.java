bd45476b2677fa9a44778d9b5e9137f8e560d01f
ThanhBT <105102395+kjjjoP3@users.noreply.github.com>
2024-09-11 23:40:27 +0700

    update code fl mentor

--- src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
+++ src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
@@ -19,48 +19,47 @@ import javax.inject.Inject;
  */
 final class ProfilerImpl implements Profiler {
 
-    private final Clock clock;
-    private final ProfilingState state = new ProfilingState();
-    private final ZonedDateTime startTime;
+	private final Clock clock;
+	private final ProfilingState state = new ProfilingState();
+	private final ZonedDateTime startTime;
 
-    @Inject
-    ProfilerImpl(Clock clock) {
-        this.clock = Objects.requireNonNull(clock, "Clock must not be null.");
-        this.startTime = ZonedDateTime.now(clock);
-    }
+	@Inject
+	ProfilerImpl(Clock clock) {
+		this.clock = Objects.requireNonNull(clock, "Clock must not be null.");
+		this.startTime = ZonedDateTime.now(clock);
+	}
 
-    @Override
-    public <T> T wrap(Class<T> klass, T delegate) throws IllegalArgumentException {
-        Objects.requireNonNull(klass, "Class must not be null.");
-        if (!Arrays.stream(klass.getDeclaredMethods())
-                .anyMatch(method -> method.isAnnotationPresent(Profiled.class))) {
-            throw new IllegalArgumentException("Profiled annotation not found in " + klass.getName());
-        }
+	@Override
+	public <T> T wrap(Class<T> klass, T delegate) throws IllegalArgumentException {
+		Objects.requireNonNull(klass, "Class must not be null.");
+		if (!Arrays.stream(klass.getDeclaredMethods()).anyMatch(method -> method.isAnnotationPresent(Profiled.class))) {
+			throw new IllegalArgumentException("Profiled annotation not found in " + klass.getName());
+		}
 
-        // Create a dynamic proxy to wrap the delegate with ProfilingMethodInterceptor
-        ProfilingMethodInterceptor handler = new ProfilingMethodInterceptor(clock, state, delegate);
-        Object proxy = Proxy.newProxyInstance(delegate.getClass().getClassLoader(),
-                new Class<?>[] { klass }, handler);
-        return klass.cast(proxy);
-    }
+		// Create a dynamic proxy to wrap the delegate with ProfilingMethodInterceptor
+		ProfilingMethodInterceptor handler = new ProfilingMethodInterceptor(clock, state, delegate);
+		Object proxy = Proxy.newProxyInstance(delegate.getClass().getClassLoader(), new Class<?>[] { klass }, handler);
+		return klass.cast(proxy);
+	}
 
-    @Override
-    public void writeData(Path path) {
-        // Write the ProfilingState data to the specified file path, appending if the file exists
-        try (Writer writer = Files.newBufferedWriter(path, StandardOpenOption.APPEND)) {
-            writeData(writer);
-        } catch (IOException e) {
-            // Handle exception (could log it or rethrow)
-            System.err.println("Error writing profiling data: " + e.getMessage());
-        }
-    }
+	@Override
+	public void writeData(Path path) {
+		// Write the ProfilingState data to the specified file path, creating the file
+		// if it does not exist
+		try (Writer writer = Files.newBufferedWriter(path, StandardOpenOption.CREATE, StandardOpenOption.APPEND)) {
+			writeData(writer);
+		} catch (IOException e) {
+			// Handle exception (could log it or rethrow)
+			System.err.println("Error writing profiling data: " + e.getMessage());
+		}
+	}
 
-    @Override
-    public void writeData(Writer writer) throws IOException {
-        // Write the start time and profiling state to the writer
-        writer.write("Run at " + DateTimeFormatter.RFC_1123_DATE_TIME.format(startTime));
-        writer.write(System.lineSeparator());
-        state.write(writer);
-        writer.write(System.lineSeparator());
-    }
+	@Override
+	public void writeData(Writer writer) throws IOException {
+		// Write the start time and profiling state to the writer
+		writer.write("Run at " + DateTimeFormatter.RFC_1123_DATE_TIME.format(startTime));
+		writer.write(System.lineSeparator());
+		state.write(writer);
+		writer.write(System.lineSeparator());
+	}
 }
